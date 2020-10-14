b3b7dd9e769fa9f76f1902c04b24b01691965bef
Justin Pilapil <justin.pilapil@yahoo.com>
2020-10-14 16:49:17 -0700

    delete unused func

--- public/electron.js
+++ public/electron.js
@@ -80,10 +80,6 @@ function createWindow() {
   return createTray();
 }
 
-// onRightClick() {
-
-// }
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
