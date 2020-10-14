ff35e624360e473b54b8b78ab250e00970dc77c0
Justin Pilapil <justin.pilapil@yahoo.com>
2020-10-14 13:43:58 -0700

    add tray window options

--- public/electron.js
+++ public/electron.js
@@ -7,6 +7,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    // frame: false,
+    // resizable: false,
     webPreferences: {
       nodeIntegration: true,
     },
